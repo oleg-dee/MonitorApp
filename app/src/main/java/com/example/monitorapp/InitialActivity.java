eb82e1864025b61f37f8eee0ff4a4c121e390d07
Oleg <odenysenko@zoolatech.com>
2023-09-25 22:09:38 +0300

    version with an expiration within 1 year

--- app/src/main/java/com/example/monitorapp/InitialActivity.java
+++ app/src/main/java/com/example/monitorapp/InitialActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -28,6 +29,25 @@ public class InitialActivity extends Activity {
 
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
+
+        //1695667774386 2023-09-25 21:50
+        //1727203774386 +1 year
+        //this version is going to expire within 1 year
+        System.out.println(System.currentTimeMillis());
+        if (System.currentTimeMillis() > 1727203774386L) {
+            //immediately exit from the app
+            // Is the user running Lollipop or above?
+            if (Build.VERSION.SDK_INT >= 21) {
+                // If yes, run the fancy new function to end the app and
+                //  remove it from the task list.
+                finishAndRemoveTask();
+            } else {
+                // If not, then just end the app without removing it from
+                //  the task list.
+                finish();
+            }
+        }
+
 
     }
 }
